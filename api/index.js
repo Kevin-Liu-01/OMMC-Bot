806faaa04bd285a7308749f510da734d53a54c8e
Kevin Liu <66856750+Kevin-Liu-01@users.noreply.github.com>
2023-09-02 14:54:11 -0400

    Fix JSON.stringify issue

--- api/index.js
+++ api/index.js
@@ -44,7 +44,7 @@ module.exports = async (request, response) => {
   if (request.method === "POST") {
     const signature = request.headers["x-signature-ed25519"];
     const timestamp = request.headers["x-signature-timestamp"];
-    const rawBody = await getRawBody(request);
+    const rawBody = JSON.stringify(req.body);
 
     const isValidRequest = verifyKey(
       rawBody,
